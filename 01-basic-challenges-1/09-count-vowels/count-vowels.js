32d82d77e3f7087397d68dc7b931cbe853a70c0e
tduncan14 <nbatre1@yahoo.com>
2023-09-24 17:35:31 -0700

    keep moving forward

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,31 @@
-function countVowels() {}
+function countVowels(countVowel) {
+
+    let totalValue = 0
+
+    let a = 'a'
+    let e =  'e'
+    let I = 'i'
+    let o = 'o'
+    let u = 'u'
+    let y = 'y'
+
+    let word =  countVowel.toLowerCase().split('')
+
+
+    for(let i = 0 ; i < word.length; i++){
+
+
+
+        if(word[i] === a || word[i] === e || word[i] === I  || word[i] ===  o || word[i] === u|| word[i] === y){
+
+            totalValue += 1
+
+        }
+    }
+
+
+         return totalValue
+
+}
 
 module.exports = countVowels;
