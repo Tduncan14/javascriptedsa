b4928b94c2b3db3d3cdc28140a1ee3cd0b031acb
tduncan14 <nbatre1@yahoo.com>
2023-10-14 21:46:35 -0700

    keep moving forward

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,42 @@
-function removeDuplicates() {}
+// function removeDuplicates(arr) {
+
+//     let newArray = []
+//     let placement 
+
+
+//     if(!Array.isArray(arr)){
+//         //  turn it into array
+//     }
+
+//     else{
+
+//     }
+
+// }
+
+
+// function removeDuplicates(arr){
+//     const uniqueArr = []
+
+
+//     for( let i  = 0 ; i < arr.length; i++){
+//         if(!uniqueArr.includes(arr[i])){
+//             uniqueArr.push(arr[i])
+//         }
+//     }
+//     return uniqueArr
+// }
+
+//  second way of doing it.
+
+
+// build in data scutrue 
+
+
+function removeDuplicates(arr){
+    return  Array.from(new Set(arr))
+}
+
+
 
 module.exports = removeDuplicates;
