d4403518f18306811b8f39662bb685fe3715ea72
tduncan14 <nbatre1@yahoo.com>
2023-10-22 02:43:23 -0700

    keep moving forward

--- 02-basic-challenges-2/03-display-likes/display-likes.js
+++ 02-basic-challenges-2/03-display-likes/display-likes.js
@@ -1,3 +1,58 @@
-function displayLikes() {}
+function displayLikes(arr) {
 
-module.exports = displayLikes;
+    let reply 
+
+    let totallength = arr.length;
+
+    let subtracttotal = 0
+
+
+    if(arr.length === 0){
+        return 'no one like this post'
+    }
+
+
+    for( let i = 0; i <= arr.length; i++){
+
+        console.log(arr[i],'this is the issue')
+
+
+     if(arr.length === 1){
+            return  reply =`${arr[0]} like this post`
+        }
+    
+        else if(arr.length === 2 ){
+           return  reply =`${arr[0]} and ${arr[1]} like this post`
+        }
+        else if(arr.length === 3) {
+           return   reply = `${arr[0]},${arr[1]} and ${arr[2]} like this `
+    }
+        else if (arr.length <= 4){
+            
+        return  reply =  `${arr[0]}, ${arr[1]} and ${arr.length - 2} other like this`
+        }
+
+    
+    // if(arr.length === 0 ){
+
+    //     reply = 'no one like this post'
+
+    // }
+
+    // else if(arr.length === 1){
+    //     reply = `${arr[0]} like this post`
+    // }
+
+    // else if(arr.length === 2 ){
+    //     reply = `${arr[0]} and ${arr[1]} like this post`
+    // }
+    // else if(arr.length === 3) {
+    //     reply = `${arr[0]},`
+    // }
+}
+
+  console.log(reply,'hello')
+
+}
+
+module.exports = displayLikes
--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,29 @@
-function findMissingNumber() {}
+function findMissingNumber(arr) {
+
+    if(arr.length === 0) return 1;
+
+
+    const n = arr.length  + 1;
+
+     let expectedSum = n *(n+1)/2
+
+   
+     
+
+
+    // for(let i = 0; i < arr.length; i++)
+    // {
+    //     actualSum += arr[i]
+    // }''
+
+
+
+    const actualSum = arr.reduce((sum,num))
+
+   
+          return expectedSum - actualSum;
+
+
+}
 
 module.exports = findMissingNumber;
