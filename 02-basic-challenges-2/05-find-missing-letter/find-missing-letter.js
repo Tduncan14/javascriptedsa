c5802c9bb38c5d9b55ab2e80c9c7efa4da05410a
tduncan14 <nbatre1@yahoo.com>
2024-03-08 22:00:59 -0800

    keep moving forward

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,17 +1,36 @@
-function findMissingLetter(arr) {
+// function findMissingLetter(arr) {
     
-    const alphabet = `abcdefghijklmnopqrstuvxyzABCDEFGHIJKLMNOPQRSTUVWXYZ`
+//     const alphabet = `abcdefghijklmnopqrstuvxyzABCDEFGHIJKLMNOPQRSTUVWXYZ`
 
-    const startIndex = alphabet.indexOf(arr[0])
+//     const startIndex = alphabet.indexOf(arr[0])
 
 
-    for(let i = 0; i < arr.length; i++){
-        if(arr[i] !== alphabet[startIndex + i]){
-            return alphabet[startIndex + i]
+//     for(let i = 0; i < arr.length; i++){
+//         if(arr[i] !== alphabet[startIndex + i]){
+//             return alphabet[startIndex + i]
+//         }
+//     }
+
+//     return '';
+// }
+
+
+function findMissingLetter(arr){
+     let start = arr[0].charCodeAt(0)
+
+     console.log(start)
+
+     for( let i = 0; i < arr.length;i++){
+        const current = arr[i].charCodeAt(0);
+
+        if(current - start > 1) {
+            return String.fromCharCode(start + 1)
         }
-    }
 
-    return '';
+        start = current
+     }  
+
+     return ''
 }
 
 module.exports = findMissingLetter;
--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique-run.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique-run.js
@@ -1,6 +1,6 @@
 const areAllCharactersUnique = require('./are-all-chars-unique');
 
-const result1 = areAllCharactersUnique('abcdefg');
+const result1 = areAllCharactersUnique('abcdefga');
 const result2 = areAllCharactersUnique('abcdefgA');
 const result3 = areAllCharactersUnique('programming');
 
