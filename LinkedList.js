8857cbe5fab0b92a3c4d43b6f6add515b9eb15c7
tduncan14 <nbatre1@yahoo.com>
2024-08-30 13:48:57 -0700

    keep moving forward

--- LinkedList.js
+++ LinkedList.js
@@ -155,6 +155,32 @@ class LinkedList {
 
     }
 
+    reverse() {
+        let temp = this.head
+        this.head = this.tail
+        this.tail = temp
+
+        let next = temp.next
+        let prev = null
+
+
+
+
+
+        for (let i = 0; i < this.length; i++) {
+
+            next = temp.next
+            temp.next = prev
+            prev = temp
+            temp = next
+
+        }
+
+        return this
+    }
+
+
+
 
 }
 
