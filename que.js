43e605cbf59092c7f065b9119ff23e432f720429
tduncan14 <nbatre1@yahoo.com>
2024-09-12 12:20:17 -0700

    keep moving forward

---
+++ que.js
@@ -0,0 +1,19 @@
+
+
+class node {
+    constructor(value) {
+        this.value = value
+        this.next = null
+    }
+}
+
+class Queue {
+
+    constructor(value) {
+        const newNode = new Node(value)
+        this.first = newNode
+        this.last = newNode
+        this.length = 1
+    }
+
+}
