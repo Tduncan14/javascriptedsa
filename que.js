4888578649e60853faa62c646f077be784b30f2e
tduncan14 <nbatre1@yahoo.com>
2024-09-12 12:53:36 -0700

    keep moving forward

--- que.js
+++ que.js
@@ -10,10 +10,60 @@ class node {
 class Queue {
 
     constructor(value) {
-        const newNode = new Node(value)
+        const newNode = new node(value)
         this.first = newNode
         this.last = newNode
         this.length = 1
     }
+    enqueue(value) {
+        const newNode = new node(value)
+        if (this.length === 0) {
+            this.first = newNode
+            this.last = newNode
+        }
+        else {
+            this.last.next = newNode
+            this.last = newNode
+        }
+        this.length++
+        return this
+
+
+    }
+
+
+    dequeue() {
+        if (this.length === 0) return undefined
+        let temp = this.first
+        if (this.length === 1) {
+            this.first = null
+            this.last = null
+        }
+        else {
+            this.first = this.first.next
+            temp.next = null
+
+        }
+
+        this.length--
+        return temp
+
+
+
+    }
+
 
 }
+
+
+
+let myQueue = new Queue(4)
+myQueue.enqueue(3)
+myQueue.enqueue(7)
+myQueue.dequeue()
+
+
+
+
+console.log('this is my queue', myQueue)
+
